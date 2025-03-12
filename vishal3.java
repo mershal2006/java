046803bcb9efdc9d282c4d2e077f6db311c9c0db
mershal2006 <vishaall205@gmail.com>
2025-03-12 15:40:36 +0530

    hiii mershal

---
+++ vishal3.java
@@ -0,0 +1,15 @@
+public class vishal3
+{
+    public static void main(String[]args)
+    {
+        String firstname="Mershal";
+        String lastname="Tamilan";
+        String Fullname=firstname + " " + lastname;
+        System.out.println("Full Name: " + Fullname);
+        int a,b,c;
+        a=10;b=30;
+        c=a+b;
+        System.out.println(c);
+        System.out.println(Fullname+c);
+    }
+}
