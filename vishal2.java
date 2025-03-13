e1d09d7da69bc2835bebbaa7eb8e7fa2258d7ee5
mershal2006 <vishaall205@gmail.com>
2025-03-13 12:11:51 +0530

    hii mershal9

---
+++ vishal2.java
@@ -0,0 +1,9 @@
+public class vishal2
+{
+    public static void main(String[]args)
+    {
+        String str1="Good Morning";
+        String str2="Vishal";
+        System.out.println(str1>str2);
+    }
+}
