34bb2e914290a78a865c9dacd1b937f64904e562
mershal2006 <vishaall205@gmail.com>
2025-03-13 13:43:26 +0530

    Swap

---
+++ vishal8.java
@@ -0,0 +1,11 @@
+public class vishal8
+{
+    public static void main(String[]args)
+    {
+        int a=2,b=3,c=6;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a+",b="+b);
+    }
+}
