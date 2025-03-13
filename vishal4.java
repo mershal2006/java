fb160af0f2563f9428f82b12fe26bfc3f746a0d7
mershal2006 <vishaall205@gmail.com>
2025-03-13 11:05:57 +0530

    hii kingston

---
+++ vishal4.java
@@ -0,0 +1,19 @@
+public class vishal4
+{
+    public static void main(string[]args)
+    {
+        String Name;
+        Scanner sc=new scanner(System.in)
+        System.out.print("Enter your Name:");
+        Name=sc.nextline();
+        String Class="CSE B";
+        int Mark=98;
+        String Subject=Tamil;
+        char Grade="A";
+        System.out.println("Name" +Name);
+        System.out.println("Class" +Class);
+        System.out.println("Mark" +Mark);
+        System.out.println("Subject" +Subject);
+        System.out.println("Grade" +Grade);
+    }
+}
