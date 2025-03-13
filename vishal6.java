6de3c93d1796bc88ee676458881b9dc6e589ebf1
mershal2006 <vishaall205@gmail.com>
2025-03-13 11:50:09 +0530

    hii mershal7

---
+++ vishal6.java
@@ -0,0 +1,14 @@
+public class vishal6
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("c" +c);
+        int d=10;
+        int e=20;
+        int f=a-b;
+        System.out.println("f" +f);
+    }
+}
