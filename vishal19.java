ad343bc5b1a48f04e885416dac25fd7ad7ae242f
mershal2006 <vishaall205@gmail.com>
2025-03-14 15:17:20 +0530

    (CS21)

---
+++ vishal19.java
@@ -0,0 +1,10 @@
+public class vishal19
+{
+    public static void main(String[]args)
+    {
+        int [] a={1,2,3,4,5,7};
+        String [] Language={"Python","Java","C++"};
+        System.out.println(a[1]);
+        System.out.println(Language[2]);
+    }
+} 
