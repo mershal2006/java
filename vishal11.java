61a55fa0d077a1f7cde7d2580d7bf9b75171e93d
mershal2006 <vishaall205@gmail.com>
2025-03-14 09:44:12 +0530

    HIII

---
+++ vishal11.java
@@ -0,0 +1,16 @@
+public class vishal11
+{
+    public static void main(String[]args)
+    {
+        int Age=10;
+        if(Age>=18)
+        {
+            System.out.println("The Person is Major");
+        }
+                else
+                {
+                    System.out.println("The Person is not Major");
+                }
+    }
+}
+    
