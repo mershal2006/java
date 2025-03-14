9b7b777566657c326d7445b4acb5f2bf977c986a
mershal2006 <vishaall205@gmail.com>
2025-03-14 11:30:47 +0530

    CHENNAI

---
+++ vishal12.java
@@ -0,0 +1,19 @@
+public class vishal12
+{
+    public static void main(String[]args)
+    {
+        String a="Dhoni";
+        if(a=D)
+        {
+            if(a=h)
+            {
+                System.out.println("The String is found");
+            }
+                else
+                {
+                    System.out.println("The string is not found");
+                }
+        }
+        
+    }
+}
---
+++ vishal14.java
@@ -0,0 +1,12 @@
+public class vishal14
+{
+    public static void main(String[]args)
+{
+    int i=0;
+    while(i=10)
+    {
+        System.out.println(i);
+        i++;
+    }
+}
+}
