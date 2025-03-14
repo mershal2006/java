01b99597cf1efd1a6a963d67b89b203723f6ac8b
mershal2006 <vishaall205@gmail.com>
2025-03-14 15:16:43 +0530

    (CSK1)

---
+++ vishal18.java
@@ -0,0 +1,12 @@
+public class vishal18
+{
+    static void mymethod()
+    {
+        System.out.println("Hello");
+    }
+    public static void main(String[]args)
+    {
+     mymethod() ;  
+    }
+}
+
