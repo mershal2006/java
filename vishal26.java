4a46788b5b9663f8fdf14e29306c68b23c80b45b
mershal2006 <vishaall205@gmail.com>
2025-03-15 11:07:43 +0530

    cofeee

---
+++ vishal26.java
@@ -0,0 +1,30 @@
+public class vishal26
+{
+    public static void main(String []args)
+    {
+        int pick=3;
+        switch(pick)
+        {
+            case 1:
+                System.out.println("customer choose the demo number 1");
+                break;
+            case 2:
+                System.out.println("customer choose the demo number 2");
+                break;
+            case 3:
+                System.out.println("customer choose the demo number 3");
+                break;
+            case 4:
+                System.out.println("customer choose the demo number 4");
+                break;
+            case 5:
+                System.out.println("customer choose the demo number 5");
+                break;
+            case 6:
+                System.out.println("customer choose the demo number 6");
+                break;
+            default:
+                System.out.println("The menu number is invalid");
+        }
+    }
+}
