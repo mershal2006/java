09c2e7441f9cc9b539cc44d8d59f7abcd77e8a4a
mershal2006 <vishaall205@gmail.com>
2025-03-15 12:06:15 +0530

    scanner

---
+++ vishal27.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class vishal27
+{
+    public static void main(String []args)
+    {
+    Scanner ab= new Scanner(System.in);
+    System.out.println("Enter the username");
+    String Name= ab.nextLine();
+    System.out.println(Name);
+}
+}
