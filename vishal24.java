2de53ba75275b70251403f9254c2fd3c37971b4b
mershal2006 <vishaall205@gmail.com>
2025-03-15 10:20:11 +0530

    Inheritance

---
+++ vishal24.java
@@ -0,0 +1,23 @@
+class Student
+{
+    int a;
+}
+    class age extends Student
+    {
+       String Name; 
+    }
+    public class vishal24
+    {
+        public static void main(String[]args)
+    {
+        age a1=new age();
+        a1.a=101;
+        a1.Name="Vishal";
+        System.out.println(a1.a);
+        System.out.println(a1.Name);
+    }
+    }
+
+      
+    
+ 
